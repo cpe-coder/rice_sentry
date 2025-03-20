82c8c6c0d333f8ad970e594ef00e9b20ec1917d3
Grian Gajila <griangajila2@gmail.com>
2025-03-21 01:56:27 +0800

    chore: udpate about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,54 @@
+import Link from "next/link";
+
 export default function About() {
 	return (
-		<div className="flex items-center justify-center h-screen w-full">
-			About
+		<div className="flex bg-[url('/rice-background2.jpg')] bg-bottom bg-no-repeat bg-cover max-w-[2080px] items-center back justify-center h-screen w-full">
+			<div className=" h-screen w-full relative"></div>
+			<div className="flex absolute max-w-[1280px] w-full justify-between items-center gap-20 mx-auto">
+				<div className="flex flex-col text-wrap gap-16 backdrop-blur-sm rounded-md p-6 border">
+					<h1 className="text-4xl font-mono font-bolder text-indigo-800">
+						What is Rice Sentry?
+					</h1>
+					<p className="font-semibold text-lg text-wrap">
+						Rice Sentry is your digital plant pathologist! Our innovative web
+						application allows you to upload images of rice leaves and uses
+						advanced AI technology that was trained on custom model with an
+						impressive accurary of{" "}
+						<span className="text-indigo-800 font-bold">96%</span> to precisely
+						diagnose whether your rice plant is healthy or suffering from a
+						disease.
+					</p>
+					<p className="font-semibold text-lg text-wrap">
+						Rice Sentry will not only identify it but also provide information
+						about the type of disease affecting your crop. With Rice Sentry,
+						safeguarding your rice plants&apos; health is just a click away!
+					</p>
+				</div>
+				<div className="flex flex-col text-wrap gap-16 backdrop-blur-sm rounded-sm border p-6">
+					<h1 className="text-4xl font-mono font-bolder text-indigo-800">
+						How to Use Rice Sentry?
+					</h1>
+					<p className="font-semibold text-lg text-wrap">
+						Embark on your journey with Rice Sentry in just a few simple steps!
+						First, prepare a clear photo of your rice leaf - you can either take
+						a fresh snap or use an existing image. Navigate to the
+						&apos;Detect&apos; page on our web application and upload your
+						image. Our advanced AI will then analyze the image and provide you
+						with results.
+					</p>
+					<p className="font-semibold text-lg text-wrap">
+						Don&apos;t have a photo at hand? No worries! We&apos;ve got you
+						covered. Click{" "}
+						<Link
+							className="underline text-indigo-800 font-bold"
+							href="/disease"
+						>
+							Here
+						</Link>{" "}
+						to download a sample image and experience the power of Rice Sentry!
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,11 @@ export default function Home() {
 					<span className="font-extrabold underline p-2">RICE SENTRY</span>
 				</h3>
 				<div className="flex items-center gap-2">
-					<h1 className="text-7xl font-mono font-medium">Rice Disease</h1>
+					<h1 className="text-7xl font-mono font-bold">Rice Disease</h1>
 					<Image src={Rice} alt="rice logo" className="rotate-[30deg] w-24" />
 				</div>
-				<h1 className="text-7xl font-mono font-medium mb-10">
-					Detection System
-				</h1>
-				<p className="text-wrap max-w-[800px] font-semibold">
+				<h1 className="text-7xl font-mono font-bold mb-10">Detection System</h1>
+				<p className="text-wrap max-w-[800px] font-semibold text-base">
 					Having problems with rice disease but don&apos;t know what disease it
 					is? Don&apos;t worry, Rice Sentry got you covered with its Rice
 					Disease Detection to help you maintain your rice healty to keep your
