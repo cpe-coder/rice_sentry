2c43462ff6563a7febaadd668a991baa3641b173
Grian Gajila <griangajila2@gmail.com>
2025-03-29 15:53:49 +0800

    ready to deploy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
-import { Copyright } from "lucide-react";
-
 export function Footer() {
 	return (
-		<div className="p-10 flex justify-center items-center gap-2 font-medium text-sm z-[2000px] mt-6 border-t border-gray-400 dark:border-gray-800">
-			<Copyright />
-			@2025 Rice Sentry | Developed for Oriental Mindoro Farmers
+		<div className="p-10 flex-col justify-center items-center gap-2 font-medium text-sm z-[2000px] mt-6 border-t border-gray-400 dark:border-gray-800">
+			<h1 className="text-center text-base md:text-lg lg:text-lg">
+				<span className="">&copy;</span> 2025 RiceSentry | Developed for
+				Oriental Mindoro Farmers
+			</h1>
 		</div>
 	);
 }
