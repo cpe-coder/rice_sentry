eac12ced23607541e599f69dc7200b0fa8cf699c
Grian Gajila <griangajila2@gmail.com>
2025-04-13 21:35:13 +0800

    wip

--- src/app/(pages)/detect/page.tsx
+++ src/app/(pages)/detect/page.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import { Image as UImage, XCircle } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { useDropzone } from "react-dropzone";
 
 export default function ImageUploader() {
@@ -12,8 +14,15 @@ export default function ImageUploader() {
 	const [file, setFile] = React.useState<File | null>(null);
 	const [loading, setLoading] = React.useState(false);
 	const [result, setResult] = React.useState<{
-		name: string;
-		description: string;
+		class: string;
+		confidence: string;
+		details: {
+			Disease: string;
+			Description: string;
+			Recommendations: string;
+			Pesticide: string;
+			Guidelines: string;
+		};
 	} | null>(null);
 
 	const onDrop = React.useCallback((acceptedFiles: File[]) => {
@@ -29,10 +38,6 @@ export default function ImageUploader() {
 		onDrop,
 		accept: { "image/*": [] },
 		noClick: true,
-	});
-
-	React.useEffect(() => {
-		console.log("Result:", result);
 	});
 
 	const handlePaste = (event: React.ClipboardEvent<HTMLDivElement>) => {
@@ -77,14 +82,18 @@ export default function ImageUploader() {
 					url: "http://localhost:8000/predict",
 					data: formData,
 				});
-				const data = await response.data;
-				setResult(data);
+				if (response.status == 200) {
+					const data = await response.data;
+					setResult(data);
+					console.log(data);
+					setLoading(false);
+				}
 			} catch (error) {
 				console.error("Error uploading file:", error);
-				setResult({ name: "Error", description: "Upload failed." });
 			}
 		}
 	};
+
 	return (
 		<div className="bg-[url('/detection-background.jpg')] bg-bottom bg-no-repeat bg-cover items-center justify-center w-full min-h-screen">
 			<title>RiceSentry • Detection</title>
@@ -99,82 +108,115 @@ export default function ImageUploader() {
 					</p>
 				</div>
 
-				<div className="flex flex-col gap-2 backdrop-blur-sm px-5 py-8 rounded-lg bg-background">
-					<div
-						{...getRootProps()}
-						className={`border-[2px] border-indigo-500 border-dashed rounded-lg p-6 text-center cursor-pointer transition-all 
+				<div
+					className={`flex flex-col gap-2 backdrop-blur-sm px-5  rounded-lg bg-background ${
+						result ? "py-5" : "py-8"
+					}`}
+				>
+					{!result && (
+						<>
+							<div
+								{...getRootProps()}
+								className={`border-[2px] border-indigo-500 border-dashed rounded-lg p-6 text-center cursor-pointer transition-all 
 							${isDragActive && "bg-indigo-100/50"}`}
-						onPaste={handlePaste}
-					>
-						<input {...getInputProps()} />
-						<input
-							type="file"
-							accept="image/*"
-							className="hidden"
-							onChange={handleFileUpload}
-							id="fileUpload"
-						/>
-						{image ? (
-							<div className="relative w-full h-64 flex justify-center items-center">
-								<Image
-									src={image}
-									alt="Uploaded Preview"
-									className="rounded-lg"
-									layout="fill"
-									objectFit="contain"
+								onPaste={handlePaste}
+							>
+								<input {...getInputProps()} />
+								<input
+									type="file"
+									accept="image/*"
+									className="hidden"
+									onChange={handleFileUpload}
+									id="fileUpload"
 								/>
-								<button
-									onClick={handleRemoveImage}
-									className="absolute top-0 right-0 hover:cursor-pointer active:opacity-70 hover:bg-gray-300/10 hover:transition-all hover:duration-300 transition-all duration-300 p-2 rounded-full text-red-500"
+								{image ? (
+									<div className="relative w-full h-64 flex justify-center items-center">
+										<Image
+											src={image}
+											alt="Uploaded Preview"
+											className="rounded-lg"
+											layout="fill"
+											objectFit="contain"
+										/>
+										<button
+											onClick={handleRemoveImage}
+											className="absolute top-0 right-0 hover:cursor-pointer active:opacity-70 hover:bg-gray-300/10 hover:transition-all hover:duration-300 transition-all duration-300 p-2 rounded-full text-red-500"
+										>
+											<XCircle size={24} />
+										</button>
+									</div>
+								) : (
+									<div className="flex flex-col items-center justify-center gap-3">
+										<UImage
+											size={48}
+											className="dark:text-gray-300 text-gray-500"
+										/>
+										<p className="text-lg dark:text-gray-300 flex gap-2 text-gray-500">
+											<label
+												htmlFor="fileUpload"
+												className="font-bold text-lg hover:cursor-pointer hover:text-indigo-600 transition-all dark:text-white text-foreground duration-300 hover:transition-all hover:duration-300"
+											>
+												Upload a file
+											</label>
+											<span className="hidden md:flex lg:flex">
+												or drag and drop
+											</span>
+										</p>
+										<p className="text-xs md:text-sm lg:text-sm dark:text-gray-300 text-gray-500">
+											PNG, JPG, up to 10MB
+										</p>
+									</div>
+								)}
+							</div>
+							<div className="grid grid-cols-1 md:flex lg:flex justify-between gap-4 md:gap-0 lg:gap-0">
+								<p className="text-center py-2 md:text-start lg:text-start">
+									Click process button after uploading an image
+								</p>
+								<Button
+									className="bg-indigo-600 px-10 hover:cursor-pointer text-white hover:bg-indigo-800"
+									onClick={handleProcessImage}
+									disabled={!file || loading}
 								>
-									<XCircle size={24} />
-								</button>
+									{loading ? "Processing..." : "Process"}
+								</Button>
 							</div>
-						) : (
-							<div className="flex flex-col items-center justify-center gap-3">
-								<UImage
-									size={48}
-									className="dark:text-gray-300 text-gray-500"
-								/>
-								<p className="text-lg dark:text-gray-300 flex gap-2 text-gray-500">
-									<label
-										htmlFor="fileUpload"
-										className="font-bold text-lg hover:cursor-pointer hover:text-indigo-600 transition-all dark:text-white text-foreground duration-300 hover:transition-all hover:duration-300"
-									>
-										Upload a file
-									</label>
-									<span className="hidden md:flex lg:flex">
-										or drag and drop
-									</span>
-								</p>
-								<p className="text-xs md:text-sm lg:text-sm dark:text-gray-300 text-gray-500">
-									PNG, JPG, up to 10MB
-								</p>
-							</div>
-						)}
-					</div>
-
-					<div className="grid grid-cols-1 md:flex lg:flex justify-between gap-4 md:gap-0 lg:gap-0">
-						<p className="text-center py-2 md:text-start lg:text-start">
-							Click process button after uploading an image
-						</p>
-						<Button
-							className="bg-indigo-600 px-10 hover:cursor-pointer text-white hover:bg-indigo-800"
-							onClick={handleProcessImage}
-							disabled={!file || loading}
-						>
-							{loading ? "Processing..." : "Process"}
-						</Button>
-					</div>
+						</>
+					)}
 
 					{result && (
-						<div className="mt-4 p-4 bg-gray-100 rounded-lg">
-							<h3 className="text-lg font-semibold">Detection Result:</h3>
+						<div className="p-2  rounded-lg">
+							<div>
+								<h1 className="font-bold text-lg md:text-xl lg:text-2xl">
+									{result.details.Disease}
+								</h1>
+								<div className="flex flex-col justify-center gap-2 items-center">
+									<h1 className="text-base">Accuracy</h1>
+									<div className="w-16 ">
+										<CircularProgressbar
+											value={Number(result.confidence)}
+											text={`${result.confidence}%`}
+											background
+											backgroundPadding={8}
+											styles={buildStyles({
+												backgroundColor: "#4f39f6",
+												textColor: "#fff",
+												textSize: "16",
+												pathColor: "#fff",
+												trailColor: "#010314",
+											})}
+										/>
+									</div>
+								</div>
+							</div>
+
 							<p>
-								<strong>Name:</strong> {result.name}
+								<strong>Description:</strong> {result.confidence}
 							</p>
 							<p>
-								<strong>Description:</strong> {result.description}
+								<strong>Description:</strong> {result.confidence}
+							</p>
+							<p>
+								<strong>Description:</strong> {result.confidence}
 							</p>
 						</div>
 					)}
